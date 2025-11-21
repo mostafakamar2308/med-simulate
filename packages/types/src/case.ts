1f9a0e5abc759d492cc3b338d4af5a233ec80829
mostafakamar2308 <moustafa.31011197@med.tanta.edu.eg>
2025-11-21 20:33:12 +0200

    feat: initialization of types for users, cases, examinations

---
+++ packages/types/src/case.ts
@@ -0,0 +1,142 @@
+export type Self = {
+  id: string;
+  title: string;
+  complaint: string;
+
+  category: CaseCategory;
+  speciality: CaseSpeciality;
+
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type Row = {
+  id: string;
+  title: string;
+  complaint: string;
+  category: CaseCategory;
+  speciality: CaseSpeciality;
+  created_at: Date;
+  updated_at: Date;
+};
+
+export enum CaseCategory {
+  ER = 0,
+  Inpatient = 1,
+  Outpatient = 2,
+}
+
+export enum CaseSpeciality {
+  IM = 0,
+  Pediatrics = 1,
+  OBGYN = 2,
+}
+
+export enum Gender {
+  Male = 0,
+  Female = 1,
+}
+
+export type CasePersonalData = {
+  id: string;
+  caseId: string;
+  name: string;
+  age: number;
+  gender: Gender;
+  weight: number;
+  height: number;
+};
+
+export type CasePersonalDataRow = {
+  id: string;
+  case_id: string;
+  name: string;
+  age: number;
+  gender: Gender;
+  weight: number;
+  height: number;
+};
+
+export type CaseVitalSigns = {
+  id: string;
+  caseId: string;
+  respiratoryRate: number;
+  heartRate: number;
+  bloodPressure: string;
+  o2Saturation: number;
+  temperature: number;
+};
+
+export type CaseVitalSignsRow = {
+  id: string;
+  case_id: string;
+  respiratory_rate: number;
+  heart_rate: number;
+  blood_pressure: string;
+  o2_saturation: number;
+  temperature: number;
+};
+
+export type CaseExamination = {
+  id: string;
+  caseId: string;
+  templateId: string;
+};
+
+export type CaseExaminationRow = {
+  id: string;
+  case_id: string;
+  template_id: string;
+};
+
+export type CaseFindings = {
+  id: string;
+  caseExaminationId: string;
+  fieldId: string;
+  valueId: string;
+  notes?: string;
+};
+
+export type CaseFindingsRow = {
+  id: string;
+  case_examination_id: string;
+  field_id: string;
+  value_id: string;
+  notes?: string;
+};
+
+export type CaseManagementProtocol = {
+  id: string;
+  caseId: string;
+  immediateProtocol: string[];
+  drugProtocol: Record<
+    string,
+    {
+      name: string;
+      dose: string;
+      frequency: string;
+      route: string;
+    }
+  >;
+  nonDrugProtocol: string[];
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type CaseManagementProtocolRow = {
+  id: string;
+  case_id: string;
+  immediate_protocol: string[];
+  drug_protocol: Record<
+    string,
+    {
+      name: string;
+      dose: string;
+      frequency: string;
+      route: string;
+    }
+  >;
+  non_drug_protocol: string[];
+  created_at: Date;
+  updated_at: Date;
+};
--- packages/types/src/user.ts
+++ packages/types/src/user.ts
@@ -1,3 +1,91 @@
 export type Self = {
+  id: string;
   name: string;
+  whatsappNumber: string;
+  university: University;
+  createdAt: string;
+  updatedAt: string;
 };
+
+export type Row = {
+  id: string;
+  name: string;
+  whatsapp_number: string;
+  university: University;
+  created_at: Date;
+  updated_at: Date;
+};
+
+export enum University {
+  QasrElEiny = 0,
+  Helwan = 1,
+  Alexandria = 2,
+  AinShams = 3,
+  Assiut = 4,
+  Mansoura = 5,
+  AlAzhar = 6,
+  Cairo = 7,
+  Damietta = 8,
+  Fayoum = 9,
+  Minia = 10,
+  Benha = 11,
+  BeniSuef = 12,
+  KafrElSheikh = 13,
+  Menoufia = 14,
+  SouthValley = 15,
+  SuezCanal = 16,
+  Tanta = 17,
+  Zagazig = 18,
+  ZagazigFaqous = 19,
+  Sohag = 20,
+  PortSaid = 21,
+  Aswan = 22,
+  Suez = 23,
+  ArmedForces = 24,
+  NewValley = 25,
+  Luxor = 26,
+  Arish = 27,
+  SadatCity = 28,
+
+  // Private
+  NewGiza = 29,
+  October6 = 30,
+  MUST = 31,
+  NGU = 32,
+  NUB = 33,
+  BUC = 34,
+  MTI = 35,
+  Delta = 36,
+  Merit = 37,
+  Horus = 38,
+  Badya = 39,
+  MIU = 40,
+  Future = 41,
+  AhramCanadian = 42,
+  Sinai = 43,
+  NewMansoura = 44,
+  AAST = 45,
+  AlAlamein = 46,
+
+  // National
+  Galala = 47,
+  KingSalman = 48,
+  AlexandriaNational = 49,
+  AssiutNational = 50,
+  BenhaNational = 51,
+  EastPortSaidNational = 52,
+  MansouraNational = 53,
+  MiniaNational = 54,
+  NewIsmailiaNational = 55,
+  SouthValleyNational = 56,
+  ZagazigNational = 57,
+  BeniSuefNational = 58,
+  MenoufiaNational = 59,
+  CairoNational = 60,
+  AinShamsNational = 61,
+  SohagNational = 62,
+  KafrElSheikhNational = 63,
+  TantaNational = 64,
+  FayoumNational = 65,
+  HelwanNational = 66,
+}
